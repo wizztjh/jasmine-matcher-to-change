bde4fc435b34eb070b36dc973aba73da82423fe8
wizztjh <wizztjh@gmail.com>
2014-07-19 14:59:43 +0800

    Added spec to check changeFunction changes result of valueFunction
    - fixed require string of jasmine matcher to change

--- test/jasmine-matcher-to-change-spec.js
+++ test/jasmine-matcher-to-change-spec.js
@@ -1,9 +1,19 @@
-require('../src/jasmine-matcher-to-chnage');
+"use strict"
+require('../src/jasmine-matcher-to-change');
 
 describe('jasmine matcher toChange', function(){
   describe('when action function changes the return value of the value function', function(){
 
     it('returns true', function(){
+      function valueFunction(){
+        return value
+      }
+      function changeFunction(){
+        value = "changed text"
+      }
+      var value = "original text";
+
+      expect(changeFunction).toChange(valueFunction);
     });
 
   });
